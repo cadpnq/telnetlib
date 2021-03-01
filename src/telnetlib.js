47b784c27b8a5f703963702486d3270cdf1de34e
Nicky Nickell <pnqcad@gmail.com>
2021-03-01 12:53:05 -0500

    Add secure telnet

--- src/telnetlib.js
+++ src/telnetlib.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const tls = require('tls');
 const TelnetSocket = require('./TelnetSocket/TelnetSocket');
 const constants = require('./constants');
 
@@ -23,7 +24,30 @@ function createConnection(options, handler) {
   return telnet;
 }
 
+function createSecureServer(options, handler) {
+  return tls.createServer(options, (c) => {
+    const telnet = new TelnetSocket(c, options);
+    handler(telnet);
+    telnet.negotiate()
+    .catch((e) => {
+      console.log(e);
+    });
+  });
+}
+
+function createSecureConnection(options, handler) {
+  const connection = tls.connect(options, handler);
+  const telnet = new TelnetSocket(connection, options);
+  telnet.negotiate()
+  .catch((e) => {
+    console.log(e);
+  });
+  return telnet;
+}
+
 exports.createServer = createServer;
 exports.createConnection = createConnection;
+exports.createSecureServer = createSecureServer;
+exports.createSecureConnection = createSecureConnection;
 exports.options = constants.options;
 exports.constants = constants;
